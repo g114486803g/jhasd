4785a8c5b4c16480a38320e3f4d02eddd11000f4
g114486803g <48343071+g114486803g@users.noreply.github.com>
2019-08-16 21:56:19 +0800

    Update ingress.sh

--- ingress/ingress.sh
+++ ingress/ingress.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-docker pull registry.cn-shenzhen.aliyuncs.com/jbjb/k8s-jbs:nginx-ingress-controlle
+docker pull registry.cn-shenzhen.aliyuncs.com/jbjb/k8s-jbs:nginx-ingress-controller
 
 docker tag registry.cn-shenzhen.aliyuncs.com/jbjb/k8s-jbs:nginx-ingress-controller quay.io/kubernetes-ingress-controller/nginx-ingress-controller:0.25.0 
 
-docker rmi registry.cn-shenzhen.aliyuncs.com/jbjb/k8s-jbs:nginx-ingress-controlle
+docker rmi registry.cn-shenzhen.aliyuncs.com/jbjb/k8s-jbs:nginx-ingress-controller
