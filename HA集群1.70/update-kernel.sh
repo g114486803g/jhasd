c25b6993baeb50e68bc00d380939a3d9614a8f72
g114486803g <48343071+g114486803g@users.noreply.github.com>
2019-12-15 02:51:55 +0800

    Create update-kernel.sh

---
+++ HA集群1.70/update-kernel.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+rpm -Uvh http://mirror.rc.usf.edu/compute_lock/elrepo/kernel/el7/x86_64/RPMS/elrepo-release-7.0-4.el7.elrepo.noarch.rpm 
+yum clean all
+
+## 安装最新的主线稳定内核
+yum --enablerepo=elrepo-kernel install kernel-ml-devel -y
+yum --enablerepo=elrepo-kernel install kernel-ml -y
+
+sed -i 's/saved/0/' /etc/default/grub
+
+grub2-mkconfig -o /boot/grub2/grub.cfg
+
+uname -sr
+reboot
