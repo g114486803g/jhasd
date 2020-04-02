75066f8578712ad7790c392f7c0a38a765a09412
g114486803g <48343071+g114486803g@users.noreply.github.com>
2020-04-02 10:08:52 +0800

    Update master.sh

--- master.sh
+++ master.sh
@@ -1,8 +1,21 @@
+------- 手动输入这里然后删除---------------------
+cat <<EOF > /etc/sysconfig/modules/ipvs.modules
+#!/bin/sh
+ipvs_mods_dir="/usr/lib/modules/$(uname -r)/kernel/net/netfilter/ipvs/"
+for mod in `ls ${ipvs_mods_dir} | grep -o "^[^.]*"`; do
+    /sbin/modprobe $mod
+done
+
+EOF
+
+-------------------------
+
 #!/bin/bash
 
 #yum源配置
 systemctl stop firewalld.service 
 systemctl disable firewalld.service
+
 
 cat <<EOF > /etc/yum.repos.d/kubernetes.repo
 [kubernetes]
@@ -15,7 +28,8 @@ gpgkey=https://mirrors.aliyun.com/kubernetes/yum/doc/yum-key.gpg
        https://mirrors.aliyun.com/kubernetes/yum/doc/rpm-package-key.gpg
 EOF
 
-wget https://download.docker.com/linux/centos/docker-ce.repo -P /tmp
+yum install ipset ipvsadm wget bash-completion.noarch -y
+wget https://download.docker.com/linux/centos/docker-ce.repo -P /etc/yum.repos.d/
 
 # 关闭selinux
 setenforce 0
@@ -32,6 +46,12 @@ vm.swappiness=0
 EOF
 sysctl --system
 sysctl -p /etc/sysctl.d/k8s.conf
+
+chmod +x /etc/sysconfig/modules/ipvs.modules && /etc/sysconfig/modules/ipvs.modules
+
+lsmod | grep -e ip_vs -e nf_conntrack_ipv4
+cut -f1 -d " "  /proc/modules | grep -e ip_vs -e nf_conntrack_ipv4
+lsmod | grep ip_vs
 
 clear 
 echo "-----------我的淫荡的分割线-------------------------"
@@ -57,9 +77,10 @@ systemctl restart docker
 echo "稍等2秒准备开始初始化集群"
 sleep 2
 
-kubeadm init --image-repository registry.aliyuncs.com/google_containers --kubernetes-version=v1.14.1 --pod-network-cidr=10.244.0.0/16
+kubeadm init --image-repository registry.aliyuncs.com/google_containers  --pod-network-cidr=10.244.0.0/16
 
 echo "如果得到了令牌请记住"
 
-echo "安装flannel网卡"
-kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
+echo "下载flannel网卡"
+wget  https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
+ls flannel
